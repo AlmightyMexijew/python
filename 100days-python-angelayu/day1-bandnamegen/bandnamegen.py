88436f86c6a0e140c5bde52e52b26b856a6766c1
almightymexijew <ctepper625@gmail.com>
2023-12-25 21:15:00 +0200

    day 1

---
+++ 100days-python-angelayu/day1-bandnamegen/bandnamegen.py
@@ -0,0 +1,5 @@
+print("Welcome to Gershon's Band Name Generator")
+userCity = input("What's the name of the city you grew up in?")
+userPet = input("What's your pet's name?")
+Output = f'Your possible band name is {userCity} {userPet}'
+print(Output)
