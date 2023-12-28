e1e413593222db9d80d7801396e4560960525905
almightymexijew <ctepper625@gmail.com>
2023-12-28 21:16:08 +0200

    more day 2

---
+++ 100days-python-angelayu/day2-lifeinweeks/lifeinweeks.py
@@ -0,0 +1,9 @@
+#set the amount of weeks in a year
+year = int(52)
+#get the amount of years
+years = int(input("How old are you in years?"))
+#calculate
+weeks = (years * year)
+#print
+print(weeks)
+print(f'You are {weeks} weeks old')
