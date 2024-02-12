595c23a25a303ece731da4a4fedbd65244006e68
almightymexijew <ctepper625@gmail.com>
2024-02-12 13:47:06 +0200

    switching to angela yu method for simplicity of code

--- 100days-python-angelayu/day7-hangman/hangman.py
+++ 100days-python-angelayu/day7-hangman/hangman.py
@@ -1,26 +1,16 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
-lives = 5
+
 #Randomly choose a word from the word_list and assign to variable chosen_word
 chosen_word = random.choice(word_list)
 
 #Ask user to guess a letter and assign it to guess, making all guesses lowercase
-def make_a_guess():
-    guess = str(input("Choose a letter:  "))
-    guess = guess.lower()
-    if len(guess) > 1:
-        print("Your guess is too big. Pick again")
-        make_a_guess()
-    else:
-        print(guess)
-        
-#Check if the letter guessed is one of the letters in the chosen_word
-def in_word():
-    if guess in chosen_word:
-        print("It's there")
-    else:
-        print("Not there")
-        lives -= 1
-        print(lives)
+guess = input("Guess a letter: ").lower()
 
-make_a_guess()
+#Check if the letter guessed is one of the letters in the chosen_word
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
