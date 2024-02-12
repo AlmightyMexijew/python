02c79fd2d26912d133de2ad31a0177807e021499
almightymexijew <ctepper625@gmail.com>
2024-02-12 14:11:15 +0200

    letter display and filling

--- 100days-python-angelayu/day7-hangman/hangman.py
+++ 100days-python-angelayu/day7-hangman/hangman.py
@@ -3,14 +3,27 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #Randomly choose a word from the word_list and assign to variable chosen_word
 chosen_word = random.choice(word_list)
-
+#Create empty list called display. Add _ to display
+display = []
+for x in range(len(chosen_word)):
+    display.append("_")
+print(display)
 #Ask user to guess a letter and assign it to guess, making all guesses lowercase
-guess = input("Guess a letter: ").lower()
+def make_a_guess():
+    guess = input("Guess a letter: ").lower()
+    letter_check(guess)
 
 #Check if the letter guessed is one of the letters in the chosen_word
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+def letter_check(guess):
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            print("Right")
+            display[position] = (chosen_word[position])
+        else:
+            print("Wrong")
+    print(display)
+    make_a_guess()
 
+
+make_a_guess()
