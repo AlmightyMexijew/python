6287668e6aedd164aba855ca1b551753ba18e63f
almightymexijew <ctepper625@gmail.com>
2024-03-18 10:27:40 +0200

    number guess game

---
+++ pythonplay/numguess/numguess.py
@@ -0,0 +1,20 @@
+import random
+
+ran_num = random.randint(0,10)
+num_arr = []
+
+guess = 0
+while guess < 5:
+    guess_input = input("Pick a number between 1 and 10.  ")
+    if guess_input == ran_num:
+        print(f"You win! The number was {ran_num}")
+        break
+    else:
+        print("Guess again you're wrong")
+        guess = guess + 1
+        num_arr.append(guess_input)
+        print(f"Current guess count is {guess}")
+        print(f"Current guesses are {num_arr}")
+        continue
+
+print("Game over")
