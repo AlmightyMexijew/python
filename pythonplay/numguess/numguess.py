cb578ae64d35dfe951f02ab01923e327cc094b4a
almightymexijew <ctepper625@gmail.com>
2024-03-18 10:33:58 +0200

    fixed == test to fit data type strictness, test proved it works by getting a win now

--- pythonplay/numguess/numguess.py
+++ pythonplay/numguess/numguess.py
@@ -5,16 +5,16 @@ num_arr = []
 
 guess = 0
 while guess < 5:
-    guess_input = input("Pick a number between 1 and 10.  ")
-    if guess_input == ran_num:
+    guess_input = input("Pick a number between 1 and 10. \n")
+    if int(guess_input) == ran_num:
         print(f"You win! The number was {ran_num}")
         break
     else:
-        print("Guess again you're wrong")
+        print("Guess again you're wrong \n")
         guess = guess + 1
         num_arr.append(guess_input)
-        print(f"Current guess count is {guess}")
-        print(f"Current guesses are {num_arr}")
+        print(f"Current guess count is {guess} \n")
+        print(f"Current guesses are {num_arr} \n")
         continue
 
 print("Game over")
